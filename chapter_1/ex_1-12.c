aa8415a2b5b710d97d3aa2ad2e305a61e7fe2bf8
jcalendo <jcalendo@gmail.com>
2020-07-07 20:04:20 -0400

    added ex 12 solution

--- chapter_1/ex_1-12.c
+++ chapter_1/ex_1-12.c
@@ -3,6 +3,7 @@
 #define IN  1
 #define OUT 0
 
+/* Write a program that prints its input one word per line*/
 int main()
 {
     int c, state;
@@ -10,9 +11,12 @@ int main()
     state = IN;
     while ((c = getchar()) != EOF)
     {
-        if (c != ' ' || c != '\n')
+        if (c == ' ' || c == '\n')
+        {
+            putchar('\n');
+        } else
         {
             putchar(c);
-        }
+        }        
     }
 }
