5f6ec6ab3669b0b14053455eb195b84efbe53498
jcalendo <jcalendo@gmail.com>
2020-07-07 20:30:03 -0400

    modified ex 12

--- chapter_1/ex_1-12.c
+++ chapter_1/ex_1-12.c
@@ -1,17 +1,14 @@
 #include <stdio.h>
 
-#define IN  1
-#define OUT 0
 
 /* Write a program that prints its input one word per line*/
 int main()
 {
-    int c, state;
+    int c;
 
-    state = IN;
     while ((c = getchar()) != EOF)
     {
-        if (c == ' ' || c == '\n')
+        if (c == ' ' || c == '\n' || c == '\t')
         {
             putchar('\n');
         } else
