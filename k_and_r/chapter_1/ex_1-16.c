b0a0cdb7c5b2f5f7736247967bcb9857ecbb0f86
jcalendo <jcalendo@gmail.com>
2020-08-27 20:36:32 -0400

    restructured

--- k_and_r/chapter_1/ex_1-16.c
+++ k_and_r/chapter_1/ex_1-16.c
@@ -1,13 +1,10 @@
 #include <stdio.h>
-
 #define MAXLINE 1000
 
 int get_line(char line[], int maxline);
 void copy(char to[], char from[]);
 
-/* Revise the main routine of the longest line program so that it will correctly 
-print the length of arbitrarily long input lines and as much as possible of the
-test */
+/* print longest input line */
 int main()
 {
     int len;
@@ -17,49 +14,35 @@ int main()
 
     max = 0;
     while ((len = get_line(line, MAXLINE)) > 0)
-    {
-        if (len > max)
-        {
+        if (len > max) {
             max = len;
             copy(longest, line);
         }
-    }
-    
     if (max > 0)
-    {
         printf("%s", longest);
-    }
-
-    return 0;
 }
 
+/* getline: read a line into s, return length */
 int get_line(char s[], int lim)
 {
     int c, i;
 
-    for(i = 0; i < lim - 1 && ((c = getchar()) != EOF && c != '\n'); ++i)
+    for (i=0; i<lim-1 && (c = getchar()) != EOF && c !='\n'; ++i)
         s[i] = c;
-
-    if (c == '\n')
-    {
+    if (c == '\n') {
         s[i] = c;
-        ++i;
-    }
-    else if(c == EOF && i > 0)
-    {
-        s[i] = '\n'; 
         ++i;
     }
     s[i] = '\0';
-    
     return i;
 }
 
+/* copy: copy 'from' into 'to'; assume to is big enough */
 void copy(char to[], char from[])
 {
     int i;
 
     i = 0;
-    while((to[i] = from[i]) != '\0')
+    while ((to[i] = from[i]) != '\n')
         ++i;
 }
