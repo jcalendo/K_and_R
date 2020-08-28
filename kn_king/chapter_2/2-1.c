4171b767ac03b7f971c208dfcd0e011ed407bfc1
jcalendo <jcalendo@gmail.com>
2020-08-27 21:21:55 -0400

    added knking

---
+++ kn_king/chapter_2/2-1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+#define C '*'
+
+/* Write a program that prints a check mark to the screen */
+int main(void)
+{
+  printf("%8c\n", C);
+  printf("%7c\n", C);
+  printf("%6c\n", C);
+  printf("%c%4c\n", C, C);
+  printf("%2c%2c\n", C, C);
+  printf("%3c\n", C);
+
+  return 0;
+}
+ 
